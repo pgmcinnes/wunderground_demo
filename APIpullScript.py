c61ee10801c2b26e766ce9a58a49b08e59d19bac
pgmcinnes <pgmcinnes@gmail.com>
2017-09-22 17:41:33 -0400

    Initial Commit
    Some descriptors

---
+++ APIpullScript.py
@@ -0,0 +1,14 @@
+"""
+Created on Fri Sep 22 17:35:47 2017
+
+@author: Garrett McInnes
+
+Demo script pulling data utilizing the Weather Underground API
+
+Target City is Philadelphia
+
+"""
+
+#API_key = 20b0a508fdef779b
+#project_name='WUnderground API'
+from lxml import html
