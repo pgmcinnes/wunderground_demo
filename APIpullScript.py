858f5b14ac32cdd169213a19bcb4d3bfd4031136
pgmcinnes <pgmcinnes@gmail.com>
2017-09-24 18:41:27 -0400

    Temperature and Conditions called
    Completed call to API for temp and conditions

--- APIpullScript.py
+++ APIpullScript.py
@@ -5,10 +5,24 @@ Created on Fri Sep 22 17:35:47 2017
 
 Demo script pulling data utilizing the Weather Underground API
 
-Target City is Philadelphia
+Target City is Philadelphia, PA
 
 """
 
 #API_key = 20b0a508fdef779b
 #project_name='WUnderground API'
-from lxml import html
+from urllib2 import urlopen
+import json
+
+weather = urlopen('http://api.wunderground.com/api/20b0a508fdef779b/geolookup/conditions/q/PA/Philadelphia.json')
+weather_json = weather.read()
+parsed_json = json.loads(weather_json)
+city = parsed_json['location']['city']
+state = parsed_json['location']['state']
+current_temp = parsed_json['current_observation']['temp_f']
+feels_like = parsed_json['current_observation']['feelslike_f']
+current_conditions = parsed_json['current_observation']['weather']
+
+
+print('Current temperature in %s, %s: %s deg F' % (city,state, current_temp))
+print('It is currently %s in %s, %s.' % (current_conditions,city,state))
